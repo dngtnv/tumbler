78693a0b02441f5ac5225dbc1433d505483c4c54
dngtnv <kakalupin@gmail.com>
2022-08-12 17:57:10 +0700

    Implement API endpoint

--- client/main.js
+++ client/main.js
@@ -1,165 +1,165 @@
-const inputElement = document.querySelector('#search-input');
-const lightboxContainer = document.querySelector('.lightbox-container');
-const lightboxImage = document.querySelector('.lightbox-img');
-const lightboxBtns = document.querySelectorAll('.lightbox-btn');
-const loader = document.querySelector('.loader');
+const inputElement = document.querySelector('#search-input')
+const lightboxContainer = document.querySelector('.lightbox-container')
+const lightboxImage = document.querySelector('.lightbox-img')
+const lightboxBtns = document.querySelectorAll('.lightbox-btn')
+const loader = document.querySelector('.loader')
 
-let scrollLock = false;
+let scrollLock = false
 
 const showLightBox = () => {
-  lightboxContainer.classList.add('active');
-};
+  lightboxContainer.classList.add('active')
+}
 
 lightboxContainer.addEventListener('click', () => {
-  lightboxContainer.classList.remove('active');
-});
+  lightboxContainer.classList.remove('active')
+})
 
 const getTotalPosts = async blogName => {
-  const response = await fetch(`/user/${blogName}/total_posts`);
-  const data = await response.json();
-  return data.total;
-};
+  const response = await fetch(`/user/${blogName}/total_posts`)
+  const data = await response.json()
+  return data.total
+}
 
 const getProfile = async blogName => {
-  let profile;
+  let profile
   try {
-    const response = await fetch(`/user/${blogName}`);
-    const data = await response.json();
-    profile = { avatar: data.info[0].avatar, username: data.info[0].username, description: data.info[0].description };
-    loadProfile(profile);
-    getImages(blogName);
+    const response = await fetch(`/user/${blogName}`)
+    const data = await response.json()
+    profile = { avatar: data.info[0].avatar, username: data.info[0].username, description: data.info[0].description }
+    loadProfile(profile)
+    getImages(blogName)
   } catch (err) {
-    profile = { avatar: '', username: 'Not Found', description: '' };
-    loadProfile(profile);
+    profile = { avatar: '', username: 'Not Found', description: '' }
+    loadProfile(profile)
   }
-};
+}
 
 const loadProfile = profile => {
-  const avatarElement = document.querySelector('#avatar');
-  const nameElement = document.querySelector('#name');
-  const desElement = document.querySelector('#description');
-  avatarElement.src = profile.avatar;
-  nameElement.innerHTML = profile.username;
-  desElement.innerHTML = profile.description;
-};
+  const avatarElement = document.querySelector('#avatar')
+  const nameElement = document.querySelector('#name')
+  const desElement = document.querySelector('#description')
+  avatarElement.src = profile.avatar
+  nameElement.innerHTML = profile.username
+  desElement.innerHTML = profile.description
+}
 
 const getImages = async (blogName, offset) => {
   try {
-    displayLoading();
+    displayLoading()
     if (!offset) {
-      offset = 0;
+      offset = 0
     }
-    const response = await fetch(`/${blogName}/photos/20/offset=${offset}`);
-    const data = await response.json();
-    loadImages(data);
+    const response = await fetch(`/user/${blogName}/photos/20/offset=${offset}`)
+    const data = await response.json()
+    loadImages(data)
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
-};
+}
 
 const loadImages = imagesArr => {
-  let html = '';
+  let html = ''
   imagesArr.images.forEach(image => {
-    html += `<div class="image-item lightbox-enabled">`;
-    html += `<img id="${image.id}" src="${image.og_size}" data-imagesrc="${image.og_size}" alt="${image.alt}">`;
-    html += `</div>`;
-  });
-  displayLoading();
-  document.querySelector('.image-list').insertAdjacentHTML('beforeend', html);
+    html += `<div class="image-item lightbox-enabled">`
+    html += `<img id="${image.id}" src="${image.og_size}" data-imagesrc="${image.og_size}" alt="${image.alt}">`
+    html += `</div>`
+  })
+  displayLoading()
+  document.querySelector('.image-list').insertAdjacentHTML('beforeend', html)
   /**************lightbox***************/
-  let lightboxEnabled = document.querySelectorAll('.lightbox-enabled');
-  let lightboxArray = [...lightboxEnabled];
-  let lastImage = lightboxArray.length - 1;
-  let activeImg;
+  let lightboxEnabled = document.querySelectorAll('.lightbox-enabled')
+  let lightboxArray = [...lightboxEnabled]
+  let lastImage = lightboxArray.length - 1
+  let activeImg
 
   const setActiveImage = image => {
     if (!image) {
-      return;
+      return
     } else {
-      lightboxImage.src = image.firstChild.dataset.imagesrc;
+      lightboxImage.src = image.firstChild.dataset.imagesrc
     }
-    activeImg = lightboxArray.indexOf(image);
-  };
+    activeImg = lightboxArray.indexOf(image)
+  }
   lightboxBtns.forEach(btn => {
     btn.addEventListener('click', e => {
-      e.stopPropagation();
-      slidesHandler(e.currentTarget.id);
-    });
-  });
+      e.stopPropagation()
+      slidesHandler(e.currentTarget.id)
+    })
+  })
   const slidesHandler = moveItem => {
-    moveItem.includes('left') ? slideLeft() : slideRight();
-  };
+    moveItem.includes('left') ? slideLeft() : slideRight()
+  }
   const slideLeft = () => {
     // activeImg === 0 ? setActiveImage(lightboxArray[lastImage]) : setActiveImage(lightboxArray[activeImg].previousElementSibling);
     if (activeImg === 0) {
-      setActiveImage(lightboxArray[lastImage]);
+      setActiveImage(lightboxArray[lastImage])
     } else if (!lightboxArray[activeImg]) {
-      return;
+      return
     } else {
-      setActiveImage(lightboxArray[activeImg].previousElementSibling);
+      setActiveImage(lightboxArray[activeImg].previousElementSibling)
     }
-  };
+  }
   const slideRight = () => {
     // activeImg === lastImage ? setActiveImage(lightboxArray[0]) : setActiveImage(lightboxArray[activeImg].nextElementSibling);
     if (activeImg === lastImage) {
-      setActiveImage(lightboxArray[0]);
+      setActiveImage(lightboxArray[0])
     } else if (!lightboxArray[activeImg]) {
-      return;
+      return
     } else {
-      setActiveImage(lightboxArray[activeImg].nextElementSibling);
+      setActiveImage(lightboxArray[activeImg].nextElementSibling)
     }
-  };
+  }
   lightboxEnabled.forEach(img => {
     img.addEventListener('click', e => {
-      showLightBox();
-      setActiveImage(img);
-    });
-  });
+      showLightBox()
+      setActiveImage(img)
+    })
+  })
   window.addEventListener('keydown', e => {
-    if (!lightboxContainer.classList.contains('active')) return;
+    if (!lightboxContainer.classList.contains('active')) return
     if (e.key.includes('Left') || e.key.includes('Right')) {
-      e.preventDefault();
-      slidesHandler(e.key.toLowerCase());
+      e.preventDefault()
+      slidesHandler(e.key.toLowerCase())
     }
-  });
+  })
   /*****************************/
-  scrollLock = false;
-};
+  scrollLock = false
+}
 
 const displayLoading = () => {
-  loader.style.display = 'block';
+  loader.style.display = 'block'
   setTimeout(() => {
-    loader.style.display = 'none';
-  }, 1800);
-};
+    loader.style.display = 'none'
+  }, 1800)
+}
 
 const init = () => {
-  let searchValue;
-  let totalPosts;
+  let searchValue
+  let totalPosts
   inputElement.addEventListener('keydown', e => {
     if (e.keyCode === 13) {
-      searchValue = inputElement.value;
-      let lowerInput = searchValue.toLowerCase();
-      inputElement.blur();
-      let element = document.querySelector('.image-list');
+      searchValue = inputElement.value
+      let lowerInput = searchValue.toLowerCase()
+      inputElement.blur()
+      let element = document.querySelector('.image-list')
       while (element.firstChild) {
-        element.removeChild(element.firstChild);
+        element.removeChild(element.firstChild)
       }
-      getProfile(lowerInput);
-      getTotalPosts(lowerInput).then(data => (totalPosts = data));
+      getProfile(lowerInput)
+      getTotalPosts(lowerInput).then(data => (totalPosts = data))
       window.onscroll = function () {
-        if (scrollLock) return;
+        if (scrollLock) return
         if (this.innerHeight + this.pageYOffset >= (document.body.scrollHeight / 4) * 3 && element.hasChildNodes()) {
-          scrollLock = true;
-          let postElement = document.querySelectorAll('.image-item');
+          scrollLock = true
+          let postElement = document.querySelectorAll('.image-item')
           if (postElement.length === totalPosts) {
-            return;
+            return
           } else {
-            getImages(lowerInput, postElement.length);
+            getImages(lowerInput, postElement.length)
           }
         }
-      };
+      }
     }
-  });
-};
-init();
+  })
+}
+init()
--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+  require('dotenv').config()
 }
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const tumblr = require('tumblr.js');
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+const tumblr = require('tumblr.js')
+const app = express()
+const port = process.env.PORT || 3000
 const client = tumblr.createClient({
   credentials: {
     consumer_key: process.env.CONSUMER_KEY,
@@ -15,96 +15,96 @@ const client = tumblr.createClient({
     token_secret: process.env.TOKEN_SECRET,
   },
   returnPromises: true,
-});
+})
 
-app.use(express.static(path.resolve(__dirname, '../client/dist')));
-app.use(cors());
+app.use(express.static(path.resolve(__dirname, '../client/dist')))
+app.use(cors())
 const errorHandler = (err, req, res, next) => {
-  console.log(`Error: ${err.message}`);
-  const status = err.status || 404;
-  res.status(status).json({ message: err.message });
-};
+  console.log(`Error: ${err.message}`)
+  const status = err.status || 404
+  res.status(status).json({ message: err.message })
+}
 
 const getUserInfo = blogName => {
-  const info = [];
+  const info = []
   const response = client
     .blogInfo(blogName)
     .then(data => {
-      const blog = { avatar: data.blog.avatar[0].url, name: data.blog.name, description: data.blog.description };
-      info.push({ avatar: blog.avatar, username: blog.name, description: blog.description });
-      return info;
+      const blog = { avatar: data.blog.avatar[0].url, name: data.blog.name, description: data.blog.description }
+      info.push({ avatar: blog.avatar, username: blog.name, description: blog.description })
+      return info
     })
     .catch(err => {
-      throw new Error(err.message);
-    });
-  return response;
-};
+      throw new Error(err.message)
+    })
+  return response
+}
 const getImageUrls = (blogName, postNumber, offsetNumber) => {
-  let imageUrls = [];
+  let imageUrls = []
   const response = client
     .blogPosts(blogName, { type: 'photo', limit: postNumber, offset: offsetNumber })
     .then(data => {
       data.posts.filter(post => {
         if (post['type'] === 'photo') {
-          imageUrls.push({ id: post.id, og_size: post.photos[0].original_size.url, alt: post.summary });
+          imageUrls.push({ id: post.id, og_size: post.photos[0].original_size.url, alt: post.summary })
         } else {
-          let str = post.trail[0].content;
-          let start = str.indexOf('src');
-          let end = str.indexOf('alt');
-          let url = str.slice(start + 5, end - 2);
-          imageUrls.push({ id: post.id, og_size: url, alt: post.summary });
+          let str = post.trail[0].content
+          let start = str.indexOf('src')
+          let end = str.indexOf('alt')
+          let url = str.slice(start + 5, end - 2)
+          imageUrls.push({ id: post.id, og_size: url, alt: post.summary })
         }
-      });
-      return imageUrls;
+      })
+      return imageUrls
     })
     .catch(err => {
-      throw new Error(err.message);
-    });
-  return response;
-};
+      throw new Error(err.message)
+    })
+  return response
+}
 const getTotalPosts = blogName => {
   const response = client
     .blogPosts(blogName, { type: 'photo' })
     .then(data => {
-      const total = data.total_posts;
-      return total;
+      const total = data.total_posts
+      return total
     })
     .catch(err => {
-      throw new Error(err.message);
-    });
-  return response;
-};
+      throw new Error(err.message)
+    })
+  return response
+}
 
 app.get('/user/:username', async (req, res, next) => {
   try {
-    const data = await getUserInfo(req.params.username);
-    res.status(200).json({ info: data });
+    const data = await getUserInfo(req.params.username)
+    res.status(200).json({ info: data })
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
-app.get('/:user/photos/:number/offset=:offset', async (req, res, next) => {
+})
+app.get('/user/:username/photos/:number/offset=:offset', async (req, res, next) => {
   try {
-    const data = await getImageUrls(req.params.user, +req.params.number, +req.params.offset);
-    res.status(200).json({ images: data });
+    const data = await getImageUrls(req.params.username, +req.params.number, +req.params.offset)
+    res.status(200).json({ images: data })
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 app.get('/user/:username/total_posts', async (req, res, next) => {
   try {
-    const data = await getTotalPosts(req.params.username);
-    res.status(200).json({ total: data });
+    const data = await getTotalPosts(req.params.username)
+    res.status(200).json({ total: data })
   } catch (err) {
-    next(err);
+    next(err)
   }
-});
+})
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
-});
-app.use(errorHandler);
+  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
+})
+app.use(errorHandler)
 
 app.listen(port, () => {
-  console.log(`The server is listening at http://localhost:${port}`);
-});
+  console.log(`The server is listening at http://localhost:${port}`)
+})
